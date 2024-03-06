102c4037afe7e8591bf26fb0f5b91f6b7bdb2964
Oliver Günther <mail@oliverguenther.de>
2024-03-06 13:29:52 +0100

    Prefix relative url root for health checks (#93)

---
+++ spec/charts/openproject/relative_url_root_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe 'relative url root configuration' do
+  let(:template) { HelmTemplate.new(default_values) }
+
+
+  context 'when setting openproject railsRelativeUrlRoot' do
+    let(:default_values) do
+      HelmTemplate.with_defaults(<<~YAML
+        openproject:
+          railsRelativeUrlRoot: '/foobar'
+      YAML
+      )
+    end
+
+    it 'adds that as a prefix to the health checks', :aggregate_failures do
+      %w[livenessProbe readinessProbe].each do |probe|
+        path = template.find_container('Deployment/optest-openproject-web', 'openproject').dig(probe, 'httpGet', 'path')
+        expect(path).to eq '/foobar/health_checks/default'
+      end
+    end
+  end
+
+  context 'when setting no relative URL root' do
+    let(:default_values) do
+      {}
+    end
+
+    it 'adds no prefix', :aggregate_failures do
+      %w[livenessProbe readinessProbe].each do |probe|
+        path = template.find_container('Deployment/optest-openproject-web', 'openproject').dig(probe, 'httpGet', 'path')
+        expect(path).to eq '/health_checks/default'
+      end
+    end
+  end
+end
