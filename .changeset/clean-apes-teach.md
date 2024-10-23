9a71b288e8cd53a749b299d9cf07b2fd0a6f73b9
Oliver Günther <mail@oliverguenther.de>
2024-10-23 15:14:56 +0200

    Allow unsetting the host name env OPENPROJECT_HOST__NAME  (#148)
    * Do not always output OPENPROJECT_HOST__NAME
    
    This would break a tenant switching deployment
    
    * Allow unsetting the host name env

---
+++ .changeset/clean-apes-teach.md
@@ -0,0 +1,5 @@
+---
+"@openproject/helm-charts": minor
+---
+
+Allow unsetting the host name env
---
+++ spec/charts/openproject/host_name_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe 'host name configuration' do
+  let(:template) { HelmTemplate.new(default_values) }
+
+  subject { template.dig('Secret/optest-openproject-core', 'stringData') }
+
+  context 'when setting host' do
+    let(:default_values) do
+      HelmTemplate.with_defaults(<<~YAML
+        openproject:
+          host: bla.example.com
+      YAML
+      )
+    end
+
+    it 'adds a respective ENV', :aggregate_failures do
+      expect(subject)
+        .to include("OPENPROJECT_HOST__NAME" => "bla.example.com")
+    end
+  end
+
+  context 'when setting no host name but leaving ingress enabled' do
+    let(:default_values) do
+      HelmTemplate.with_defaults({})
+    end
+
+    it 'the host name is the default', :aggregate_failures do
+      expect(subject)
+        .to include("OPENPROJECT_HOST__NAME" => "openproject.example.com")
+    end
+  end
+
+  context 'when setting no host name and disabling ingress' do
+    let(:default_values) do
+      HelmTemplate.with_defaults(<<~YAML
+        ingress:
+          enabled: false
+      YAML
+      )
+    end
+
+    it 'the host is not output', :aggregate_failures do
+      expect(subject.keys)
+        .not_to include("OPENPROJECT_HOST__NAME")
+    end
+  end
+end
